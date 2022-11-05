d04a0dd0fc877c6c8965f15498d7215c2e160a12
abdelaziz <abdelaazizeelhathoute.2018@gmail.com>
2022-11-05 18:06:44 +0100

    jhjh

---
+++ pages/instruments.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../fonteAwsome/css/fontawesome.min.css">
+    
+    
+    <title>Instruments</title>
+</head>
+<body>
+   <?php include('menu.php');?>
+   <p>hello</p>
+  <!-- solid style -->
+  <i class="fa-solid fa-user"></i>
+
+  <!-- regular style -->
+  <i class="fa-regular fa-user"></i>
+
+  <!-- light style -->
+  <i class="fa-light fa-user"></i>
+
+  <!-- duotone style -->
+  <i class="fa-duotone fa-user"></i>
+
+  <!-- all new thin style -->
+  <i class="fa-thin fa-user"></i>
+
+  <!-- all new sharp family -->
+  <i class="fa-sharp fa-solid fa-user"></i>
+
+  <!--brand icon-->
+  <i class="fa-brands fa-github-square"></i>
+</body>
+</html>
